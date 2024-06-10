224a2676a0f8361adf1a4610553bb4c37b5d2898
ADIII <61045979+DAHOTOYIII@users.noreply.github.com>
2024-06-10 21:58:41 +0800

    PEP 8 formatting adjustments

--- src/cloudword/__main__.py
+++ src/cloudword/__main__.py
@@ -6,17 +6,19 @@ from tabulate import tabulate
 
 ASSETS_FOLDER = "src/cloudword/inputs/"
 
+
 def convert_to_table(table_obj):
     table = tabulate(table_obj, headers="keys")
     with open("output.txt", "w") as f:
         f.write(table)
     return table
 
+
 def rank_words(word_list):
-    rank_words =[]
+    rank_words = []
     max_frequent_word = 100
     for idx, word in enumerate(word_list):
-        percentage = (word['count']/max_frequent_word) * 100
+        percentage = (word['count'] / max_frequent_word) * 100
         if idx == 0:
             rank = 'Huge'
             max_frequent_word = word['count']
@@ -25,11 +27,12 @@ def rank_words(word_list):
         elif percentage > 30:
             rank = 'Normal'
         else:
-            rank = 'small'
-        rank_words.append({'word' : word['word'],
+            rank = 'Small'
+        rank_words.append({'word': word['word'],
                            'count': word['count'],
-                           'size' : rank})
+                           'size': rank})
     return rank_words
+
 
 def get_words_from_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -38,7 +41,7 @@ def get_words_from_file(file_path):
         return words
 
 
-def main(folder_path, num_top_words : int):
+def main(folder_path, num_top_words: int):
     word_counter = Counter()
     # Iterate through each file in the folder
     for filename in os.listdir(folder_path):
@@ -46,7 +49,7 @@ def main(folder_path, num_top_words : int):
         if os.path.isfile(file_path) and filename.endswith('.txt'):
             words = get_words_from_file(file_path)
             word_counter.update(words)
-   
+
     word_counts = [{'word': word, 'count': count} for word, count in word_counter.most_common(num_top_words)]
     ranked_word = rank_words(word_counts)
     table = convert_to_table(ranked_word)
@@ -54,7 +57,6 @@ def main(folder_path, num_top_words : int):
     return table
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     top_words = 40
     main(ASSETS_FOLDER, top_words)
